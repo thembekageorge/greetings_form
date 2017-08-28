1a5c8e449e1af19103fdae1611d49b9098e330d7
thembekageorge <thembiegeorge@gmail.com>
2017-08-28 15:19:39 +0200

    first commit.

---
+++ index.js
@@ -0,0 +1,59 @@
+
+const express = require('express' );
+const exphbs = require( "express-handlebars" );
+const form = require('body-parser' );
+var app = express();
+
+// setting rendering engine
+app. engine ("handlebars" , exphbs ({
+defaultLayout : "main" ,
+extname : "handlebars"
+}));
+app. use(express. static( "public" ));
+app. use(form. urlencoded({
+extended : false
+}));
+app. set( "view engine" , "handlebars" )
+app. get( "/" , function (req, res) {
+res. render ("home" );
+});
+
+// app.post("/",function(req,res){
+//  greeting = 'Hello, ' + req.body.name + '!';
+//   res.render('home', {
+//     greetingMsg: greet
+//   });
+// });
+
+var greeting = "" ;
+var nameList = [];
+var noDuplication = [];
+app. post ( "/" , function(req, res) {
+var nameEntered = req.body.name ;
+
+var language = req.body.language ;
+
+if (language === "isiXhosa" ) {
+greeting = "Molo, " + nameEntered;
+} else if (language === "English" ) {
+greeting = "Hello, " +nameEntered;
+} else if (language === "Sesotho" ) {
+greeting = "Dumela, " + nameEntered;
+}
+if (noDuplication.indexOf(nameEntered) === -1) {
+  noDuplication.push(nameEntered);
+}
+nameList.push(nameEntered);
+console.log(nameList);
+console.log(noDuplication);
+res. render ("home" , {
+greetingMsg : greeting
+});
+});
+
+//start the server
+var server = app. listen( 3000 , function () {
+var host = server. address().address;
+var port = server. address(). port ;
+console. log( 'Example app listening at http://%s:%s' , host, port);
+});
