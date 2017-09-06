049dea9b6ecfdcde4460157db8c1de34794925d9
thembekageorge <thembiegeorge@gmail.com>
2017-09-06 12:49:22 +0200

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const exphbs = require("express-handlebars");
 const form = require('body-parser');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dbs1');
+// const mongoURL = process.env.MONGO_DB_URL || "'mongodb://localhost/dbs1'";
+//
+// mongoose.connect(mongoURL);
 
 // setting rendering engine
 app.engine("handlebars", exphbs({
@@ -49,23 +54,29 @@ app.post("/", function(req, res) {
   console.log(nameList);
   console.log(noDuplication);
 
-  // for (var i = 0; i < nameList.length; i++) {
-  //   if (map[nameList[i]] != null) {
-  //     map[nameList[i]] += 1;
-  //   } else {
-  //     map[nameList[i]] = 1;
-  //   }
-  //   // console.log('counter : ' + map[nameList[i]])
-  // }
+  for (var i = 0; i < nameList.length; i++) {
+    if (map[nameList[i]] != null) {
+      map[nameList[i]] += 1;
+    } else {
+      map[nameList[i]] = 1;
+    }
+    // console.log('counter : ' + map[nameList[i]])
+  }
   console.log('counter : ' + map[nameList[i]])
   res.render("home", {
     greetingMsg: greeting
   });
 });
 
+app.get('/greeted', function(req, res) {
+    res.render('greeted', {
+        greeted: noDuplication
+    });
+});
+
 //start the server
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
